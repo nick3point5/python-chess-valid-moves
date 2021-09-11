a093d0b8b5364e2a9807a14850f98e890cd93f79
nick3point5 <kenny.trinh4444@gmail.com>
2021-09-11 11:46:17 -0500

    clear git

--- .env/Scripts/activate.bat
+++ .env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\ktt44\Desktop\Code Stuff\chess\.env
+set VIRTUAL_ENV=C:\Users\ktt44\code\side-bits\python-chess-valid-moves\.env
 
 if not defined PROMPT set PROMPT=$P$G
 
